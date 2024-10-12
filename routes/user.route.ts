da1d1666ba4ad73799872070a4a4a4d6f54939f4
Lethal15004 <Phamvahuy2004@gmail.com>
2024-10-12 16:58:43 +0700

    Sửa lại trang Đăng nhập admin

--- routes/user.route.ts
+++
@@ -1,8 +0,0 @@
-import express,{Router} from 'express';
-const router : Router = express.Router();
-
-import * as userController from '../controller/user.controller';
-
-router.get('/login',userController.login);
-
-export default router;
